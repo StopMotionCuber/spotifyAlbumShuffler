377c6d012fc8f2c807cc3bb27a8d0287b52753d1
StopMotionCuber <ruben.simons@web.de>
2021-10-31 00:58:51 +0200

    Initial commit

---
+++ spotifyAlbumShuffler/apps/spotify/model.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class SpotifyUser(models.Model):
+    id = models.AutoField()
+    spotify_user_id = models.CharField(100)
+
+
+class SpotifyPlaylist(models.Model):
+    id = models.AutoField()
+    spotify_playlist_id = models.CharField(50)
+    owner = models.ForeignKey(SpotifyUser, on_delete=models.CASCADE)
+    back_to_back = models.BooleanField()
+    last_snapshot = models.CharField(100)
+    albums_included = models.ManyToManyField("SpotifyAlbum")
+    playlist_name = models.CharField(200)
+    playlist_picture_uri = models.URLField()
+
+
+class SpotifyAlbum(models.Model):
+    id = models.AutoField()
+    spotify_album_id = models.CharField(50)
+    album_name = models.CharField(200)
+    album_cover = models.URLField()
+
+
+class SpotifyTokenInformation(models.Model):
+    id = models.AutoField()
