b840ff9a3b932cdad19cb99816f745f1ee29a896
StopMotionCuber <ruben.simons@web.de>
2022-01-05 23:28:22 +0100

    Update model to newest requirements

--- spotifyAlbumShuffler/spotify/models.py
+++ spotifyAlbumShuffler/spotify/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class SpotifyUser(models.Model):
     id = models.AutoField(primary_key=True)
     spotify_user_id = models.CharField(max_length=100, unique=True)
-    django_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=100)
     token = models.JSONField(null=True, blank=True)
     last_interaction = models.DateTimeField(auto_now=True)
 
@@ -15,11 +15,12 @@ class SpotifyPlaylist(models.Model):
     spotify_playlist_id = models.CharField(max_length=50, unique=True)
     owner = models.ForeignKey(SpotifyUser, on_delete=models.CASCADE)
     back_to_back = models.BooleanField()
-    enabled = models.BooleanField()
+    enabled = models.BooleanField(default=False)
     last_snapshot = models.CharField(max_length=100)
     albums_included = models.ManyToManyField("SpotifyAlbum")
     playlist_name = models.CharField(max_length=200)
     playlist_picture_url = models.URLField(blank=True)
+    last_playlist_picture_update = models.DateTimeField(blank=True, null=True)
 
 
 class SpotifyAlbum(models.Model):
